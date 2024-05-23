6bdf721c1b12a7aae47c6bfd4cfc5d139a935139
Roman-0S <romansteele2020@gmail.com>
2024-05-23 13:06:04 -0400

    When going to a new page, will now scroll to the top / made sidebarblogcard now link to specific blog / made PagedListExtension

---
+++ BlazorBlog.Client/Models/PagedList.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorBlog.Client.Models
+{
+    public class PagedList<T> where T : class
+    {
+        public int Page { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int TotalItems { get; set; }
+
+        public IEnumerable<T> Data { get; set; } = [];
+    }
+}
